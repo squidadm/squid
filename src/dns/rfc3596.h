86006681b1e3d8779530172827358b70997f5098
SquidAdm <noc@lists.squid-cache.org>
2023-10-21 06:14:44 +0000

    Source Format Enforcement

--- src/dns/rfc3596.h
+++ src/dns/rfc3596.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1996-2023 The Squid Software Foundation and contributors
+ * Copyright (C) 1996-2024 The Squid Software Foundation and contributors
  *
  * Squid software is distributed under GPLv2+ license and includes
  * contributions from numerous individuals and organizations.
@@ -13,41 +13,41 @@
 #include "dns/rfc1035.h"
 
 ssize_t rfc3596BuildAQuery(const char *hostname,
-                                        char *buf,
-                                        size_t sz,
-                                        unsigned short qid,
-                                        rfc1035_query * query,
-                                        ssize_t edns_sz);
+                           char *buf,
+                           size_t sz,
+                           unsigned short qid,
+                           rfc1035_query * query,
+                           ssize_t edns_sz);
 
 ssize_t rfc3596BuildAAAAQuery(const char *hostname,
-        char *buf,
-        size_t sz,
-        unsigned short qid,
-        rfc1035_query * query,
-        ssize_t edns_sz);
+                              char *buf,
+                              size_t sz,
+                              unsigned short qid,
+                              rfc1035_query * query,
+                              ssize_t edns_sz);
 
 ssize_t rfc3596BuildPTRQuery4(const struct in_addr,
-        char *buf,
-        size_t sz,
-        unsigned short qid,
-        rfc1035_query * query,
-        ssize_t edns_sz);
+                              char *buf,
+                              size_t sz,
+                              unsigned short qid,
+                              rfc1035_query * query,
+                              ssize_t edns_sz);
 
 ssize_t rfc3596BuildPTRQuery6(const struct in6_addr,
-        char *buf,
-        size_t sz,
-        unsigned short qid,
-        rfc1035_query * query,
-        ssize_t edns_sz);
+                              char *buf,
+                              size_t sz,
+                              unsigned short qid,
+                              rfc1035_query * query,
+                              ssize_t edns_sz);
 
 /* RFC3596 library implements RFC1035 generic host interface */
 ssize_t rfc3596BuildHostQuery(const char *hostname,
-        char *buf,
-        size_t sz,
-        unsigned short qid,
-        rfc1035_query * query,
-        int qtype,
-        ssize_t edns_sz);
+                              char *buf,
+                              size_t sz,
+                              unsigned short qid,
+                              rfc1035_query * query,
+                              int qtype,
+                              ssize_t edns_sz);
 
 /* RFC3596 section 2.1 defines new RR type AAAA as 28 */
 #define RFC1035_TYPE_AAAA 28
